bdbc1bff7008051cecf10a1af6e5dd544698717e
Github0008VP <vishu200217@gmail.com>
2026-06-26 15:18:42 +0530

    adding cors

--- server/src/app.js
+++ server/src/app.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const productRoutes = require('./routes/product.routes.js')
+const cors = require('cors')
 
 const app = express();
 
+app.use(cors({
+    origin: "https://code-vector-task-one.vercel.app"
+}))
 
 app.get("/", (req, res) => {
     res.send("hi, server is live......")
